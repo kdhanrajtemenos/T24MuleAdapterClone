f8d1cad0a6eb4d322273e9a0e06add5a8af858df
Dinesh Kumar <dkumar@temenos.com>
2017-02-20 12:57:25 +0000

    removed the stale directory which was failed attempt to correct the project name. Angel will try this again soon

--- t24-outbound-connector/src/main/java/com/temenos/adapter/mule/T24outbound/metadata/model/MetadataDiscoveryService.java
+++
@@ -1,5 +0,0 @@
-package com.temenos.adapter.mule.T24outbound.metadata.model;
-
-public abstract interface MetadataDiscoveryService {
-	 public abstract OutboundMetadataDiscoveryService getOutboundService();
-}
--- t24-outbound-connector/src/main/java/com/temenos/adapter/mule/T24outbound/metadata/model/MetadataModel.java
+++
@@ -1,8 +0,0 @@
-package com.temenos.adapter.mule.T24outbound.metadata.model;
-
-import java.util.Map;
-
-public interface MetadataModel {
-    public String verify();
-    public Map<String, String> getAllSchemas(String serviceName);
-}
