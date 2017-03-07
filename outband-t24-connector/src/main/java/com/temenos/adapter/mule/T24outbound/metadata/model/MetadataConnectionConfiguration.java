aa6b6003aa8acb436b5446c4f86765b225c1b753
Dinesh Kumar <dkumar@temenos.com>
2017-03-07 12:29:33 +0000

    Removed the misspelled connector

--- outband-t24-connector/src/main/java/com/temenos/adapter/mule/T24outbound/metadata/model/MetadataConnectionConfiguration.java
+++
@@ -1,6 +0,0 @@
-package com.temenos.adapter.mule.T24outbound.metadata.model;
-
-public abstract interface MetadataConnectionConfiguration
-{
-  public abstract T24MetadataConnectionType getConnectionType();
-}
--- outband-t24-connector/src/main/java/com/temenos/adapter/mule/T24outbound/metadata/model/T24MetadataConnectionType.java
+++
@@ -1,8 +0,0 @@
-package com.temenos.adapter.mule.T24outbound.metadata.model;
-
-public enum T24MetadataConnectionType
-{
-  TAFC_AGENT,  WEB_SERVICE;
-  
-  private T24MetadataConnectionType() {}
-}
